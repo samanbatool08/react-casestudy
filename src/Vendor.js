565cf3a41be66b092b5d43096ced4209aaf37ded
Your Name <samanbatool08@gmail.com>
2020-08-26 09:48:42 -0400

    component setup

---
+++ src/Vendor.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Vendor() {
+    return (
+        <div className='vendor'>
+            <div className='vendor__addCriteria'>
+                <h1>Add Criteria</h1>
+            </div>
+
+            <div className='vendor'>
+
+            </div>
+        </div>
+    )
+}
+
+export default Vendor;
